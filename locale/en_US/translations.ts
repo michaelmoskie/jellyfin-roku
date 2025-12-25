943d617c1beddd9d9053c87da2264708b6eae2c6
Axl Nunez <axltico12@gmail.com>
2025-12-24 20:18:44 -0500

    feat: Add Recently Released section and corresponding data loading functionality

--- locale/en_US/translations.ts
+++ locale/en_US/translations.ts
@@ -133,6 +133,10 @@
             <translation>Recently Added in</translation>
         </message>
         <message>
+            <source>Recently Released</source>
+            <translation>Recently Released</translation>
+        </message>
+        <message>
             <source>On Now</source>
             <translation>On Now</translation>
         </message>
